ef2f1c17ea78314ea996dfd1acdab92494f14c75
belse-de <belse.de@gmail.com>
2017-04-07 22:47:00 +0200

    get the teensy 3.1 to translate buttonpresses to usb-keyboard

---
+++ chatpad/print.ino
@@ -0,0 +1,20 @@
+void printHex(uint8_t* buffer, int len){
+  for(int i = 0; i < len; i++){
+    uint8_t nr = buffer[i];
+    if(nr < 0x10) { Serial.print("0"); }
+    Serial.print(nr, HEX); 
+  }
+}
+
+void initWait(){
+  pinMode(13, OUTPUT);
+  digitalWrite(13, HIGH);
+  delay(500);
+  digitalWrite(13, LOW);
+  delay(500);
+  digitalWrite(13, HIGH);
+  delay(500);
+  digitalWrite(13, LOW);
+  delay(5500);
+  digitalWrite(13, HIGH);
+}
